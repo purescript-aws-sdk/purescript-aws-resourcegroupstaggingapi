70f5e65385966d773302c69358b91affb22033e4
Philippe Vinchon <pvinchon@gilt.com>
2018-03-12 18:18:13 +0000

    PureScript mapping for aws-sdk-js 2.207.0

--- src/ResourceGroupsTaggingAPI.purs
+++ src/ResourceGroupsTaggingAPI.purs
@@ -20,32 +20,40 @@ import Data.StrMap as StrMap
 import AWS.Request as Request
 import AWS.Request.Types as Types
 
-serviceName = "ResourceGroupsTaggingAPI" :: String
-
 
 -- | <p>Returns all the tagged resources that are associated with the specified tags (keys and values) located in the specified region for the AWS account. The tags and the resource types that you specify in the request are known as <i>filters</i>. The response includes all tags that are associated with the requested resources. If no filter is provided, this action returns a paginated resource list with the associated tags.</p>
 getResources :: forall eff. GetResourcesInput -> Aff (exception :: EXCEPTION | eff) GetResourcesOutput
-getResources = Request.request serviceName "getResources" 
+getResources = Request.request service method  where
+    service = Request.ServiceName "ResourceGroupsTaggingAPI"
+    method = Request.MethodName "getResources"
 
 
 -- | <p>Returns all tag keys in the specified region for the AWS account.</p>
 getTagKeys :: forall eff. GetTagKeysInput -> Aff (exception :: EXCEPTION | eff) GetTagKeysOutput
-getTagKeys = Request.request serviceName "getTagKeys" 
+getTagKeys = Request.request service method  where
+    service = Request.ServiceName "ResourceGroupsTaggingAPI"
+    method = Request.MethodName "getTagKeys"
 
 
 -- | <p>Returns all tag values for the specified key in the specified region for the AWS account.</p>
 getTagValues :: forall eff. GetTagValuesInput -> Aff (exception :: EXCEPTION | eff) GetTagValuesOutput
-getTagValues = Request.request serviceName "getTagValues" 
+getTagValues = Request.request service method  where
+    service = Request.ServiceName "ResourceGroupsTaggingAPI"
+    method = Request.MethodName "getTagValues"
 
 
 -- | <p>Applies one or more tags to the specified resources. Note the following:</p> <ul> <li> <p>Not all resources can have tags. For a list of resources that support tagging, see <a href="http://docs.aws.amazon.com/awsconsolehelpdocs/latest/gsg/supported-resources.html">Supported Resources</a> in the <i>AWS Resource Groups and Tag Editor User Guide</i>.</p> </li> <li> <p>Each resource can have up to 50 tags. For other limits, see <a href="http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/Using_Tags.html#tag-restrictions">Tag Restrictions</a> in the <i>Amazon EC2 User Guide for Linux Instances</i>.</p> </li> <li> <p>You can only tag resources that are located in the specified region for the AWS account.</p> </li> <li> <p>To add tags to a resource, you need the necessary permissions for the service that the resource belongs to as well as permissions for adding tags. For more information, see <a href="http://docs.aws.amazon.com/awsconsolehelpdocs/latest/gsg/obtaining-permissions-for-tagging.html">Obtaining Permissions for Tagging</a> in the <i>AWS Resource Groups and Tag Editor User Guide</i>.</p> </li> </ul>
 tagResources :: forall eff. TagResourcesInput -> Aff (exception :: EXCEPTION | eff) TagResourcesOutput
-tagResources = Request.request serviceName "tagResources" 
+tagResources = Request.request service method  where
+    service = Request.ServiceName "ResourceGroupsTaggingAPI"
+    method = Request.MethodName "tagResources"
 
 
 -- | <p>Removes the specified tags from the specified resources. When you specify a tag key, the action removes both that key and its associated value. The operation succeeds even if you attempt to remove tags from a resource that were already removed. Note the following:</p> <ul> <li> <p>To remove tags from a resource, you need the necessary permissions for the service that the resource belongs to as well as permissions for removing tags. For more information, see <a href="http://docs.aws.amazon.com/awsconsolehelpdocs/latest/gsg/obtaining-permissions-for-tagging.html">Obtaining Permissions for Tagging</a> in the <i>AWS Resource Groups and Tag Editor User Guide</i>.</p> </li> <li> <p>You can only tag resources that are located in the specified region for the AWS account.</p> </li> </ul>
 untagResources :: forall eff. UntagResourcesInput -> Aff (exception :: EXCEPTION | eff) UntagResourcesOutput
-untagResources = Request.request serviceName "untagResources" 
+untagResources = Request.request service method  where
+    service = Request.ServiceName "ResourceGroupsTaggingAPI"
+    method = Request.MethodName "untagResources"
 
 
 newtype AmazonResourceType = AmazonResourceType String
